63e569217ff44f6531d343f29e9d8fad158453b6
theedenpark <edenpark.acc@gmail.com>
2022-06-29 11:04:43 +0530

    added next installment dates

--- resources/views/Invoice/Dashboard/all.blade.php
+++ resources/views/Invoice/Dashboard/all.blade.php
@@ -170,7 +170,7 @@
 <script src='https://cdn.datatables.net/1.10.9/js/jquery.dataTables.min.js'></script>
 <script id="rendered-js" >
     $(document).ready(function () {
-        $('#myDataTable').DataTable({ order: [[7, 'desc']], });
+        $('#myDataTable').DataTable({ order: [[8, 'desc']], });
     });
 </script>
 @endsection
