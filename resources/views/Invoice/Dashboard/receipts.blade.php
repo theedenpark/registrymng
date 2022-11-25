5a75be073453085aefd679ad3e036f1d1a590726
theedenpark <edenpark.acc@gmail.com>
2022-11-25 11:36:11 +0530

    fggg

--- resources/views/Invoice/Dashboard/receipts.blade.php
+++
@@ -1,4 +0,0 @@
-<option selected disabled>Select Receipt</option>
-@foreach($receipts as $receipt)
-    <option value="{{$receipt->receipt_no}}">Plot: {{$receipt->plot_no}} - {{$receipt->primary_customer}} - {{$receipt->receipt_format}}{{$receipt->receipt_no}} ({{$receipt->phase}})</option>
-@endforeach
